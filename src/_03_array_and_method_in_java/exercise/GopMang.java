7bec3e5fd193b5f39d2020a1bcb729b184075a37
hoang1341995 <hoang1341995@gmail.com>
2022-04-02 15:50:06 +0700

    update bai tap

--- src/_03_array_and_method_in_java/exercise/GopMang.java
+++ src/_03_array_and_method_in_java/exercise/GopMang.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class GopMang {
     public static void main(String[] args) {
-        int arr1[] = {1,3,5,7,9};
-        int arr2[] = {2,4,6,8,10};
+        int arr1[] = {1,3,5,7,9,88};
+        int arr2[] = {2,4,6,8,10,99};
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(arr2));
         System.out.println(Arrays.toString(formatArray(arr1,arr2)));
@@ -13,8 +13,13 @@ public class GopMang {
     }
     public static int[] formatArray(int[] array1,int[] array2){
         int array3[] = new int[array1.length+array2.length];
-        int numberCheck = 0;
-        
+        for (int i = 0; i < array1.length; i++) {
+            array3[i] = array1[i];
+        }
+        int count = array1.length;
+        for (int i = 0; count < array3.length; count++,i++) {
+            array3[count] = array2[i];
+        }
         return array3;
     }
 
---
+++ src/_03_array_and_method_in_java/exercise/TimGiaTriNhoNhatTrongMang.java
@@ -0,0 +1,35 @@
+package _03_array_and_method_in_java.exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TimGiaTriNhoNhatTrongMang {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int min,size = 0;
+
+        do{
+            System.out.println("Nhập độ dài mảng");
+            size = sc.nextInt();
+            if (size>10){
+                System.out.println("Nhập lại");
+            }
+        }while(size>10);
+        int[] array = new int[size];
+        for (int i = 0; i < array.length ; i++) {
+            System.out.print("Nhập phần tử "+(i+1)+": ");
+            array[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("Phần tử nhỏ nhất là: "+formatArray(array));
+    }
+    public static int formatArray(int[] arr){
+       int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i]<min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
